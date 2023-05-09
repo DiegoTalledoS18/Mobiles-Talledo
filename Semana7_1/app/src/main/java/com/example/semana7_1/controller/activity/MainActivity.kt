45dc31914178b225c0988e41fe4226bcadb23e39
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2023-05-09 17:24:53 -0500

    adding bottom navigation view

--- Semana7_1/app/src/main/java/com/example/semana7_1/controller/activity/MainActivity.kt
+++ Semana7_1/app/src/main/java/com/example/semana7_1/controller/activity/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.semana7_1.controller.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.semana7_1.R
 import com.example.semana7_1.adapter.CharacterAdapter
+import com.example.semana7_1.controller.fragment.CharacterFavoriteList
+import com.example.semana7_1.controller.fragment.CharacterList
 import com.example.semana7_1.network.CharacterService
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,42 +21,31 @@ import retrofit2.Call
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    lateinit var characterRecyclerView: RecyclerView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item -> navigateTo(item) }
 
+    private fun navigateTo(item: MenuItem): Boolean {
+        item.isChecked = true
+        return supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, getFragmentFor(item))
+            .commit() > 0
+    }
 
+    private fun getFragmentFor(item: MenuItem): Fragment {
+        return when(item.itemId) {
+            R.id.home -> CharacterList()
+            R.id.favorites -> CharacterFavoriteList()
+            else -> CharacterList()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        characterRecyclerView=findViewById(R.id.rvCharacters)
-        loadCharacters()
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        navigateTo(navView.menu.findItem(R.id.home))
+
     }
 
-    private fun loadCharacters() {
-        val tvName=findViewById<TextView>(R.id.tvName)
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://my-json-server.typicode.com/DiegoTalledoS18/StarWars/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val characterService: CharacterService
-        characterService=retrofit.create(CharacterService::class.java)
-        characterService.getCharacters().enqueue(object:Callback<List<Character>>{
-            override fun onResponse(
-                call: Call<List<Character>>,
-                response: Response<List<Character>>
-            ) {
-                if(response.isSuccessful){
-                    val characters:List<Character> =response.body()!!
-                    characterRecyclerView.adapter=CharacterAdapter(characters,this@MainActivity)
-                    characterRecyclerView.layoutManager=LinearLayoutManager(this@MainActivity)
-                }
-            }
-
-            override fun onFailure(call: Call<List<Character>>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
 }
