1f43f40a9691e0a60b7fbc3ee75b93ebdfebadd2
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2023-05-09 23:52:37 -0500

    on item detail

--- Semana7_1/app/src/main/java/com/example/semana7_1/controller/fragment/CharacterList.kt
+++ Semana7_1/app/src/main/java/com/example/semana7_1/controller/fragment/CharacterList.kt
@@ -1,16 +1,19 @@
 package com.example.semana7_1.controller.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.semana7_1.R
 import com.example.semana7_1.adapter.CharacterAdapter
+import com.example.semana7_1.controller.activity.CharacterDetail
 import com.example.semana7_1.network.CharacterService
 import models.Character
 import retrofit2.Call
@@ -19,7 +22,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class CharacterList : Fragment() {
+class CharacterList : Fragment(),CharacterAdapter.OnItemClickListener {
     lateinit var characterRecyclerView: RecyclerView
 
     override fun onCreateView(
@@ -50,7 +53,7 @@ class CharacterList : Fragment() {
             ) {
                 if(response.isSuccessful){
                     val characters:List<Character> =response.body()!!
-                    characterRecyclerView.adapter= CharacterAdapter(characters,requireContext())
+                    characterRecyclerView.adapter= CharacterAdapter(characters,requireContext(),this@CharacterList)
                     characterRecyclerView.layoutManager= LinearLayoutManager(context)
                 }
             }
@@ -60,6 +63,13 @@ class CharacterList : Fragment() {
             }
 
         })
+    }
+
+    override fun onItemClicked(character: Character) {
+        val intent = Intent(context, CharacterDetail::class.java)
+        intent.putExtra("character", character)
+        startActivity(intent)
+
     }
 
 
